ae4529d5f27f83f3fe878c87f34294f1d80ac1be
Gambala <gambala.rus@gmail.com>
2013-06-06 18:12:03 +0400

    Updated to my params

---
+++ lib/omniauth/accounts/version.rb
@@ -0,0 +1,5 @@
+module OmniAuth
+  module Accounts
+    VERSION = "0.1"
+  end
+end
--- lib/omniauth/strategies/accounts.rb
+++ lib/omniauth/strategies/accounts.rb
@@ -2,39 +2,39 @@ require 'omniauth/strategies/oauth2'
 
 module OmniAuth
   module Strategies
-    class GoogleOauth2 < OmniAuth::Strategies::OAuth2
+    class Accounts < OmniAuth::Strategies::OAuth2
 
       # Possible scopes: userinfo.email,userinfo.profile,plus.me
-      DEFAULT_SCOPE = "userinfo.email,userinfo.profile"
+      # DEFAULT_SCOPE = "userinfo.email,userinfo.profile"
 
-      option :name, 'google_oauth2'
-      option :authorize_options, [:scope, :approval_prompt, :access_type, :state, :hd]
+      option :name, 'accounts'
+      # option :authorize_options, [:scope, :approval_prompt, :access_type, :state, :hd]
 
       option :client_options, {
-        :site          => 'https://accounts.google.com',
+        :site          => 'http://localhost:4000',
         :authorize_url => '/o/oauth2/auth',
         :token_url     => '/o/oauth2/token'
       }
 
-      def authorize_params
-        base_scope_url = "https://www.googleapis.com/auth/"
-        super.tap do |params|
-          # Read the params if passed directly to omniauth_authorize_path
-          options[:authorize_options].each do |k|
-            params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
-          end
-          scopes = (params[:scope] || DEFAULT_SCOPE).split(",")
-          scopes.map! { |s| s =~ /^https?:\/\// ? s : "#{base_scope_url}#{s}" }
-          params[:scope] = scopes.join(' ')
-          # This makes sure we get a refresh_token.
-          # http://googlecode.blogspot.com/2011/10/upcoming-changes-to-oauth-20-endpoint.html
-          params[:access_type] = 'offline' if params[:access_type].nil?
-          params[:approval_prompt] = 'force' if params[:approval_prompt].nil?
-          params[:login_hint] = request.params['login_hint'] if request.params['login_hint']
-          # Override the state per request
-          session['omniauth.state'] = params[:state] if request.params['state']
-        end
-      end
+      # def authorize_params
+      #   base_scope_url = "https://www.googleapis.com/auth/"
+      #   super.tap do |params|
+      #     # Read the params if passed directly to omniauth_authorize_path
+      #     options[:authorize_options].each do |k|
+      #       params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+      #     end
+      #     scopes = (params[:scope] || DEFAULT_SCOPE).split(",")
+      #     scopes.map! { |s| s =~ /^https?:\/\// ? s : "#{base_scope_url}#{s}" }
+      #     params[:scope] = scopes.join(' ')
+      #     # This makes sure we get a refresh_token.
+      #     # http://googlecode.blogspot.com/2011/10/upcoming-changes-to-oauth-20-endpoint.html
+      #     params[:access_type] = 'offline' if params[:access_type].nil?
+      #     params[:approval_prompt] = 'force' if params[:approval_prompt].nil?
+      #     params[:login_hint] = request.params['login_hint'] if request.params['login_hint']
+      #     # Override the state per request
+      #     session['omniauth.state'] = params[:state] if request.params['state']
+      #   end
+      # end
 
       uid{ raw_info['id'] || verified_email }
 
